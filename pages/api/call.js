e909bfcccf9ef09eca09474a07a2a1617802a621
urielgold <urig345@gmail.com>
2022-07-28 17:59:00 +0300

    Create call.js

---
+++ pages/api/call.js
@@ -0,0 +1,18 @@
+export default (req, res) => {
+  // Open Chrome DevTools to step through the debugger!
+  // debugger;
+  res.status(200).json({
+  __typename: 'GetCallOutput',
+  answered_at: '2022-06-21T07:51:55.000Z',
+  created_at: '2022-06-21T07:51:49.000Z',
+  call_uuid: 'ba62665e-0e85-11ed-861d-0242ac120002',
+  company_id: 0000,
+  direction: 'outbound',
+  duration: "30",
+  id: 222,
+  state: 'done',
+  type: 'voicemail',
+  from: '+0000000',
+  to: '111111'
+});
+};
