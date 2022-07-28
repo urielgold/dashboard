aa27eec0a18d4729864ff6b93bb65b284fc462e6
urielgold <urig345@gmail.com>
2022-07-28 17:52:11 +0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,5 +3,5 @@
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   // debugger;
-  res.status(200).json({ name: 'Hello, world!' });
+  res.status(200).json({ url: 'https://d23v1vftnxxoo2.cloudfront.net/cali-wataboi.mp3' });
 };
